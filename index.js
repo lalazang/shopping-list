416b028c11150180b89c28a2f4ca1ff8852e6992
Max Kao <mkao1@cougarmail.collin.edu>
2019-05-05 19:00:22 -0500

    edited index.js to check&uncheck items

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 $(function(){
 
     $('#js-shopping-list-form').submit(function(event){
-        event.preventDefault();
-        let newItem = $(this).find('#shopping-list-entry').val();
-        $('.shopping-list').append($(`<li>
-        <span class="shopping-item">${newItem}</span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-        </div>
-      </li>`));
-    });
+      event.preventDefault();
+      let newItem = $(this).find('#shopping-list-entry').val();
+      $('.shopping-list').append($(`
+        <li>
+          <span class="shopping-item">${newItem}</span>
+          <div class="shopping-item-controls">
+           <button class="shopping-item-toggle">
+             <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`
+      ));
 
+      $('#shopping-list-entry').val("");
+
+    });
 
     $('.shopping-list').on('click', '.shopping-item-delete', function(event){
       console.log('delete clicked');
       $(this).closest('li').remove();
-
     });
 
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+      console.log('check clicked');
+      $(this).closest('li').children('.shopping-item').toggleClass("shopping-item__checked");
 
+    });
 
 });
